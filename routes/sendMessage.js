fe924e9ffa9b00b06c5c432e4430f41faa497643
Andres Mercado <andres23.t30@gmail.com>
2026-06-13 13:09:42 -0500

    2deeeeeeejyyyyfoken

--- routes/sendMessage.js
+++ routes/sendMessage.js
@@ -1,7 +1,7 @@
-const router = require("express").Router();
-const axios = require("axios");
-
 router.post("/", async (req, res) => {
+
+  console.log("BODY RECIBIDO:");
+  console.log(req.body);
 
   try {
 
@@ -10,38 +10,36 @@ router.post("/", async (req, res) => {
       mensaje
     } = req.body;
 
+    console.log("TELEFONO:", telefono);
+    console.log("MENSAJE:", mensaje);
+
     const response =
       await axios.post(
-
         `https://graph.facebook.com/v22.0/${process.env.PHONE_NUMBER_ID}/messages`,
-
         {
           messaging_product: "whatsapp",
-
           to: telefono,
-
           type: "text",
-
           text: {
             body: mensaje
           }
         },
-
         {
           headers: {
             Authorization:
               `Bearer ${process.env.WHATSAPP_TOKEN}`
           }
         }
-
       );
+
+    console.log("META RESPONSE:");
+    console.log(response.data);
 
     res.json(response.data);
 
-  }
+  } catch (err) {
 
-  catch (err) {
-
+    console.log("ERROR META:");
     console.log(
       err.response?.data ||
       err.message
@@ -54,5 +52,3 @@ router.post("/", async (req, res) => {
   }
 
 });
-
-module.exports = router;
