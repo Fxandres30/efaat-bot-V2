bdecb077fd6790cba388ebd5eab52b14f6b2ce4e
Andres Mercado <andres23.t30@gmail.com>
2026-06-13 13:13:31 -0500

    233333n

--- routes/sendMessage.js
+++ routes/sendMessage.js
@@ -1,3 +1,6 @@
+const router = require("express").Router();
+const axios = require("axios");
+
 router.post("/", async (req, res) => {
 
   console.log("BODY RECIBIDO:");
@@ -52,3 +55,5 @@ router.post("/", async (req, res) => {
   }
 
 });
+
+module.exports = router;
